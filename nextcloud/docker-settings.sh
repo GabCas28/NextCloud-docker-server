d6c872b1ce6bd5f72a61bc4cb73c1e9c9c4d14cb
Gabriel Castro Muñoz <gabcas28@gmail.com>
2021-04-27 22:02:13 +0200

    fixed format of settings

--- nextcloud/docker-settings.sh
+++ nextcloud/docker-settings.sh
@@ -11,13 +11,13 @@ run_as() {
 
 echo "checking for Nextcloud configuration"
 {
-    run_as "php /var/www/html/occ maintenance:install 
-                --database 'mysql' 
-                --database-host '${MYSQL_HOST}'  
-                --database-name '${MYSQL_DATABASE}'  
-                --database-user '${MYSQL_USER}' 
-                --database-pass '${MYSQL_PASSWORD}' 
-                --admin-user '${MYSQL_ROOT_USER}' 
+    run_as "php /var/www/html/occ maintenance:install \
+                --database 'mysql' \
+                --database-host '${MYSQL_HOST}' \  
+                --database-name '${MYSQL_DATABASE}' \  
+                --database-user '${MYSQL_USER}' \
+                --database-pass '${MYSQL_PASSWORD}' \ 
+                --admin-user '${MYSQL_ROOT_USER}' \
                 --admin-pass '${MYSQL_ROOT_PASSWORD}'" 
 } || {
     echo "Nextcloud already Installed"
